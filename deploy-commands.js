0bed4d0cc6e15a67c3c411ce15774ee80c09495a
rayvenjm <jeremiahjmesa@gmail.com>
2021-11-16 00:38:15 +0800

    fixed possible vulnerability

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, guildId_2, token } = require('./config.json');
 
 const commands = [];
 
@@ -18,6 +18,6 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 
-rest.put(Routes.applicationGuildCommands(clientId, "895672074515935272"), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId_2), { body: commands })
 .then(() => console.log('Successfully registered application commands.'))
 .catch(console.error);
